a972c56f71e36925a37ac6e34ea02d894f0cf5bd
Ítalo Oliveira Gomes <ioliveiragomes17@gmail.com>
2025-07-22 21:58:23 -0300

    Rename AccountDAO and add Pacient DAO class
    Renamed AccountDAO.java to dao_01_account.java and updated the class name accordingly. Added a new placeholder class dao_02_pacient.java for future patient-related DAO functionality.

---
+++ app/src/main/java/com/example/fisiotracker/fisio_tracker_dao/dao_01_account.java
@@ -0,0 +1,4 @@
+package com.example.fisiotracker.fisio_tracker_dao;
+
+public class dao_01_account {
+}
--- app/src/main/java/com/example/fisiotracker/fisio_tracker_dao/dao_02_pacient.java
+++ app/src/main/java/com/example/fisiotracker/fisio_tracker_dao/dao_02_pacient.java
@@ -1,4 +1,4 @@
 package com.example.fisiotracker.fisio_tracker_dao;
 
-public class AccountDAO {
+public class dao_02_pacient {
 }
